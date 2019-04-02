fa99e33000474f8c186f0466a4dd5527d6a2e65e
DiegoPta <piedrahitadiegof@gmail.com>
2019-04-02 15:50:14 -0500

    Página del tablero centrada verticalmente

--- pages/tablero.vue
+++ pages/tablero.vue
@@ -5,31 +5,37 @@
                 <h1>OCA GAME</h1> 
             </div>
             
-            <div style="display: flex">
-                <div style="display: inline-block; width: 16em; margin: 1em">
-                    <h2 style="display: inline">Info de partida</h2>
-                    <div>
-                        <div class="bloque">Jug.</div>
-                        <div class="bloque">Color/Posición</div>
-                    </div>
-                    <div style="display: table-row" v-for="i in nroJugadores" :key="i">
-                        <div class="bloque">Jugador {{i}}</div>
+            <div style="position: relative; width: 100%; border-color: black; border-style: solid; justify-content: space-evenly">
+                <div style="vertical-align: middle; display: inline-block; width: 16em; margin: 1em">
+                    <div style="display: table-cell">
+                        <h2 style="display: table-row">Información</h2>
+                        
 
-                        <div v-if="colores[i] == 'Verde'" style="background-color: green" class="bloque">1</div>
-                        <div v-else-if="colores[i] == 'Rojo'" style="background-color: red" class="bloque">1</div>
-                        <div v-else-if="colores[i] == 'Azúl'" style="background-color: blue" class="bloque">1</div>
-                        <div v-else style="background-color: yellow" class="bloque">1</div>
+                        <div style="display: table-row">
+                            <div class="bloque">Jug.</div>
+                            <div class="bloque">Color/Posición</div>
+                        </div>
+                        
+                        <div style="display: table-row" v-for="i in nroJugadores" :key="i">
+                            <div class="bloque">Jugador {{i}}</div>
+
+                            <div v-if="colores[i] == 'Verde'" style="background-color: green" class="bloque">1</div>
+                            <div v-else-if="colores[i] == 'Rojo'" style="background-color: red" class="bloque">1</div>
+                            <div v-else-if="colores[i] == 'Azúl'" style="background-color: blue" class="bloque">1</div>
+                            <div v-else style="background-color: yellow" class="bloque">1</div>
+                        </div>
                     </div>
+                    
 
                 </div>
 
-                <div style="display: inline-block; width: 40em">
+                <div style="vertical-align: middle; display: inline-block;">
                     <div v-for="f in filas" :key="f" class="tablero">
                         <div v-for="c in columnas" :key="c" class="celdas"></div>
                     </div>
                 </div>
 
-                <div style="display: inline-block; width: 16em; margin: 1em">
+                <div style="display: inline-block; vertical-align: middle">
                     <img src="@/static/images/dado.png">
                 </div>
             </div>
